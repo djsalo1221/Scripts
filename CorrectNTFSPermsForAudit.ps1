11046094a9c30a0060325e2702908000815ff276
djsalo1221 <djsalo1221@gmail.com>
2020-04-18 20:20:33 -0400

    Add files via upload

---
+++ CorrectNTFSPermsForAudit.ps1
@@ -0,0 +1,16 @@
+﻿Install-Module -Name NTFSSecurity -RequiredVersion 4.2.3 
+Import-module NTFSSecurity -verbose
+
+$folderArray = get-Content -Path 'C:\PATH'
+
+$count = 0
+foreach ($folder in $folderArray)
+{
+  Write-Host $folder
+  cd $folder
+  cd ..
+  Disable-NTFSAccessInheritance $folder
+  remove-ntfsaccess -path $folder -Account DOMAIN\GROUP -AccessRights Delete
+  $count = $count + 1
+}
+write-host $count
